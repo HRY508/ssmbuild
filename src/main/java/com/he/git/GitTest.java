b22ea7c80a823f259abfb0f1657b110feaba9ffe
He <508816739@qq.com>
2022-06-08 19:30:14 +0800

    hot-fix second commit

--- src/main/java/com/he/git/GitTest.java
+++ src/main/java/com/he/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello Git!");
         System.out.println("hello git3");
         System.out.println("hello Git4");
+        System.out.println("hot-fix text");
     }
 }
