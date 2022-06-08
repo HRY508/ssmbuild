604c8eb869ad3702128df59adcfdbfcb08db8b22
He <508816739@qq.com>
2022-06-08 17:20:49 +0800

    second commit

---
+++ src/main/java/com/he/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.he.git;/*
+@author he
+@creat 2022-06-08-17:19
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
