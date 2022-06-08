ce2f1174c9873455484119330078573701d25e14
He <508816739@qq.com>
2022-06-08 17:21:42 +0800

    third commit

--- src/main/java/com/he/git/GitTest.java
+++ src/main/java/com/he/git/GitTest.java
@@ -6,5 +6,6 @@ package com.he.git;/*
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello git3");
     }
 }
